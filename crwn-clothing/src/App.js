49d999616ff3b01a816dcf1369625c39786be5c5
Saitama <mramged936@gmail.com>
2022-11-09 14:23:40 +0200

    adding firebase and sign in route

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routs/home/home.component.jsx';
-
 import Navigation from './routs/navigation/navigation.component.jsx';
+import SignIn from './routs/sign-in/sign-in.component.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   ) 
