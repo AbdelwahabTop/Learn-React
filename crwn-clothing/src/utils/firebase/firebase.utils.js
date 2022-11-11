f66519bbb475a65c5ef8495dc89d10685e2b518e
Saitama <mramged936@gmail.com>
2022-11-11 21:39:50 +0200

    finish Creating User Documents

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -42,5 +42,25 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
+
+  // if user data dose not exists
+  // creat / set the document with the data from userAuth in my collection
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    } catch (error) {
+      console.log('Error creating the user', error.message);
+    }
+  }
+
+  // if user data exists
+  // return userDocRef
+  return userDocRef;
 }
