ea8941c71f9c3eff4a15f0899e78500282ad2678
AbdelwahabTop <mramged936@gmail.com>
2022-11-30 07:13:27 +0200

    adding shop page and cart item

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routs/home/home.component.jsx';
 import Navigation from './routs/navigation/navigation.component.jsx';
 import Authentication from './routs/authentication/authentication.component.jsx';
+import Shop from './routs/shop/shop.component.jsx'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   ) 
--- crwn-clothing/src/routs/navigation/navigation.component.jsx
+++ crwn-clothing/src/routs/navigation/navigation.component.jsx
@@ -2,16 +2,20 @@ import { Fragment, useContext } from 'react';
 
 import { Outlet, Link } from 'react-router-dom';
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown (1).svg';
-import { UserContext } from '../../contexts/user.context';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown (1).svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
-
 const Navigation = () => {
   const { currentUser } = useContext( UserContext );
+  const { isCartOpen } = useContext( CartContext );
 
 
   return (
@@ -29,7 +33,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+          {isCartOpen && <CartDropdown />}
       </div>
       <Outlet /> 
     </Fragment>
