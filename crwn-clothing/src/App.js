d42b671f4bdc4597477f2f5c6670a8852eb8272f
AbdelwahabTop <mramged936@gmail.com>
2022-12-01 07:04:50 +0200

    created checkout page along with checkout items and cart functionality

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,14 +4,16 @@ import Home from './routs/home/home.component.jsx';
 import Navigation from './routs/navigation/navigation.component.jsx';
 import Authentication from './routs/authentication/authentication.component.jsx';
 import Shop from './routs/shop/shop.component.jsx'
+import Checkout from './routs/checkout/checkout.component.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
-        <Route path='auth' element={<Authentication />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   ) 
