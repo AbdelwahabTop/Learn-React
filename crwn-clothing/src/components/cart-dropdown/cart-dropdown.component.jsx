52d95cd344a1feaed52aeccdbddc2aeecad158db
AbdelwahabTop <mramged936@gmail.com>
2022-12-02 07:21:36 +0200

    add collection,documents and get products from firestore

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,16 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom'
 
-import { CartContext } from '../../contexts/cart.context'
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
-
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.style.scss'
+import './cart-dropdown.style.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
-
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
@@ -21,11 +20,13 @@ const CartDropdown = () => {
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        {cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)}
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
-}
+};
 
 export default CartDropdown;
--- crwn-clothing/src/routs/checkout/checkout.component.jsx
+++ crwn-clothing/src/routs/checkout/checkout.component.jsx
@@ -1,10 +1,10 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
 
-import { CartContext } from '../../contexts/cart.context'
+import { CartContext } from '../../contexts/cart.context';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
-import './checkout.style.scss'
+import './checkout.style.scss';
 
 const Checkout = () => {
   const { cartItems, cartTotal } = useContext(CartContext);
@@ -25,19 +25,15 @@ const Checkout = () => {
           <span>Price</span>
         </div>
         <div className='header-block'>
-          <span>Product</span>
-        </div>
-        <div className='header-block'>
           <span>Remove</span>
         </div>
       </div>
-
-        {cartItems.map((cartItem) => (
-            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-        ))}
-        <span className='total'>Total: ${cartTotal}</span>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className='total'>TOTAL: ${cartTotal}</div>
     </div>
   );
-}
+};
 
-export default Checkout
+export default Checkout;
